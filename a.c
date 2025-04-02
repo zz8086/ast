f141f9ade31d4f254fa2b8f16b35f3557f5995c1
zz8086 <167443710+zz8086@users.noreply.github.com>
2025-04-02 20:59:28 +0900

    Add files via upload

---
+++ a.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "json_c.c"
+
+typedef struct {
+    char name[50];
+} decl;
+
+int main(void)
+{
+    long file_size;
+    char *str;
+    int count = 0;
+
+    FILE *fp = fopen("ast.json", "r");
+
+    if (fp == NULL) 
+    {
+        printf("파일을 열지 못했습니다.\n");
+        return 1;
+    }
+    
+    fseek(fp, 0, SEEK_END);
+    file_size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    str = (char*)malloc(sizeof(char) * file_size);
+    fread(str, 1, file_size, fp);
+
+    json_value json = json_create(str);
+    json_value ext_obj = json_get(json, "ext");
+
+    int ext_len = json_len(ext_obj);
+    decl *name_array = (decl *)malloc(sizeof(decl) * ext_len);
+
+    for(int i = 0; i < ext_len; i++)
+    {
+            json_value ext_obj_array = json_get(ext_obj, i);
+            json_value decl_obj = json_get(ext_obj_array, "decl");
+            json_type decl_type = json_get_type(json_get(ext_obj_array, "decl"));
+            if(decl_type == JSON_UNDEFINED) {
+                continue;
+            }
+            else {
+                    json_value decl_type_obj = json_get(decl_obj, "type"); 
+                    json_value decl_type_type_obj = json_get(decl_type_obj, "type"); 
+                    json_value decl_type_type_type_obj = json_get(decl_type_type_obj, "type"); 
+                    json_value decl_type_type_names_obj = json_get(decl_type_type_type_obj, "names", 0); 
+                    json_type decl_type_type_names_type = json_get_type(json_get(decl_type_type_type_obj, "names", 0));
+                    if(decl_type_type_names_type == JSON_UNDEFINED) {
+                        json_value decl_type_type_type_type_obj = json_get(decl_type_type_type_obj, "type");
+                        json_value decl_type_type_type_type_names_obj = json_get(decl_type_type_type_type_obj, "names", 0);
+                        char *name = json_get_string(decl_obj, "name");
+                        strcpy(name_array[i].name, name);
+                        printf("함수 이름 : %s ", name); printf("리턴 타입 : ");
+                        json_print(decl_type_type_type_type_names_obj); putchar('\n');
+                        count++;
+                        continue;
+                    }
+                    char *name = json_get_string(decl_obj, "name");
+                    strcpy(name_array[i].name, name);
+                    printf("함수 이름 : %s ", name); printf("리턴 타입 : ");
+                    json_print(decl_type_type_names_obj); putchar('\n');
+                    count++;
+            }   
+    }
+
+    printf("총 함수 갯수 : %d \n", count);
+
+    free(str);
+    fclose(fp);
+
+    return 0;
+}
